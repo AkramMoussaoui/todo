ef1014d5a74500fc2fde424eecdd487bbc1ce813
Akram Moussaoui <akrammoussaoui14@gmail.com>
2021-04-10 10:43:51 +0100

    get users with thunk

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React, { useEffect } from "react";
 import "./App.css";
 import Todos from "./components/Todos";
 import Users from "./components/Users";
-import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addUser } from "./Redux/Actions/userActions";
 
 function App() {
   const dispatch = useDispatch();
-  const getUsers = async () => {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    dispatch(addUser(response.data));
-  };
   useEffect(() => {
-    getUsers();
+    dispatch(addUser());
   }, []);
   return (
     <div className="App">
